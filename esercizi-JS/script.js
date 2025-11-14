e31f7efcc11eed3a98c407079d19834b6c337def
mansourlahham14 <mansourlahham14@gmail.com>
2025-11-14 17:50:36 +0100

    js

---
+++ esercizi-JS/script.js
@@ -0,0 +1,138 @@
+// OPERATORI
+
+// let a = 7;
+// let  b = 5;
+
+// let somma = a + b;
+
+// console.log(somma);
+
+// let a = 10;
+// let  b = 7;
+
+// let somma = a - b;
+
+// console.log(somma);
+
+ // let a = 4;
+// let  b = 6;
+
+// let somma = a * b;
+
+// console.log(somma);
+
+// let a = 20;
+// let  b = 4;
+
+// let somma = a / b;
+
+// console.log(somma);
+
+// let a = 17;
+// let  b = 3;
+
+// let somma = a % b;
+
+// console.log(somma);
+
+
+// CONDIZIONI
+
+// let eta = 20;
+
+// if(eta>=18){
+// console.log("maggiorenne")
+// }else{
+// console.log("minorenne")
+// };
+
+// let voto = 30;
+
+// if(voto=30){
+// console.log("ottimo")
+// }else if(voto>=18){
+// console.log("buono")
+// }else{
+// console.log("insufficiente")
+// };
+
+//  let ora = 9;
+
+//  if(ora=9){
+// console.log("buongiorno")
+// }else if(ora=15){
+// console.log("buonpomeriggio")
+// }else if(voto=21){
+// console.log("buonasera")
+// }else{
+//     console.log("salve")
+// };
+
+// CICLI
+
+// for (let i = 0; i <= 20; i++) {
+// console.log("numero:", i);
+// };
+
+// for (let i = 0; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+ 
+// let i = 10;
+
+// while (i >= 1) {
+//     console.log(i);
+//     i--;
+// };
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`5 x ${i} = ${5 * i}`);
+// }
+
+// let somma = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     somma += i;
+// }
+
+// console.log("Somma totale:", somma);
+
+
+    // FUNZIONI
+
+    // function saluta(){
+    //     console.log("Ciao!")
+    // };
+
+    // function somma(a, b){
+    //   return a+b; 
+    // }
+
+    // function moltiplicata(a, b){
+    //   return a*b; 
+    // }
+
+    // function ispari(numero){
+    //     if(numero % === 0 ){
+    //         return true;
+    //     }else{
+    //         return false
+    //     }
+    // }
+
+    // function quadrato(numero){
+    //     return numero*numero;
+    // }
+
+    // function(nome, eta){
+    //     console.log("Ciao, mi chiamo ${nome} e ho ${eta} anni")
+    // }
+
+    // ARRAY
+
+    // let frutti = ["mela" , "banana" , "pera"];
+
+
+  
